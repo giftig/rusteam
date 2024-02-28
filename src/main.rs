6f1b6fda0accf952abbe022dce20ac96a65116e4
Rob Moore <giftiger.wunsch@gmail.com>
2024-02-28 08:58:27 +0000

    Initial notion integration
    The notion client is async and requires the tokio runtime.
    
    Looking at tokio, this probably suits my purposes anyway, so let's use
    it.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ pub mod models;
 pub mod steam;
 
 use std::env;
+use std::str::FromStr;
 
+use notion::NotionApi;
+use notion::ids::DatabaseId;
+use notion::models::search::DatabaseQuery;
 use steam::{SteamClient, SteamPlayerServiceHandling};
+use tokio;
 
-fn main() {
+fn query_steam() -> () {
     let api_key = env::var("STEAM_API_KEY").unwrap();
     let user_id = env::var("STEAM_USER_ID").unwrap();
 
@@ -16,4 +21,24 @@ fn main() {
     let steam_game = response.response.games.first().unwrap();
 
     println!("Game count: {:?}, first in list: {:?}", game_count, &steam_game);
+}
+
+async fn query_notion() -> () {
+    let api_key = env::var("NOTION_API_KEY").unwrap();
+    let db_id = env::var("NOTION_DATABASE_ID").unwrap();
+
+    let notion = NotionApi::new(api_key).unwrap();
+    let res = notion
+        .query_database(DatabaseId::from_str(&db_id).unwrap(), DatabaseQuery {sorts: None, filter: None, paging: None})
+        .await
+        .unwrap()
+        .results;
+
+    println!("{:?}", &res);
+}
+
+#[tokio::main]
+async fn main() {
+//    query_steam();
+//    query_notion().await;
 }
