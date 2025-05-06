47dd9d9a135b9019ce3f87ccea66b2b9b63efb70
Rob Moore <giftiger.wunsch@gmail.com>
2025-05-06 18:18:38 +0100

    Exclude owned games from released-wishlisted.sql

--- queries/released-wishlisted.sql
+++ queries/released-wishlisted.sql
@@ -11,8 +11,10 @@ FROM
   wishlist w
   LEFT JOIN steam_game sg ON w.app_id = sg.app_id
   LEFT JOIN game_details gd ON w.app_id = gd.app_id
+  LEFT JOIN owned_game og ON w.app_id = og.app_id
 WHERE
-  gd.is_released = TRUE
+  gd.is_released = TRUE AND
+  og.app_id IS NULL
 ORDER BY
   wishlisted_before_release DESC,
   w.wishlisted DESC
